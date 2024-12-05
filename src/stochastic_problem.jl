b74d8cf0a637aea09d83ab67c4a0472ccb705ac6
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-12-05 17:16:26 +0100

    started thinking about stochastic implementation

--- src/stochastic_problem.jl
+++ src/stochastic_problem.jl
@@ -1,18 +1,40 @@
-struct DiagonalNoise end
+struct DiagonalNoise{Tfunc,Tnoise}
+    func::Tfunc
+    function DiagonalNoise(func::Tfunc, ::Type{Tnoise}) where {Tfunc,Tnoise}
+        new{Tfunc,Tnoise}(func)
+    end
+end
 
-struct StochasticGrossPitaevskiiProblem{form,Tnoise,N,T1,T2,T3,T4,T5,T6,T7}
+function (σ!::DiagonalNoise)(ξ, buffer, u, p, t)
+    σ!.func(buffer, u, p, t)
+    randn!(ξ)
+    ξ .*= buffer
+end
+
+struct StochasticGrossPitaevskiiProblem{N,T1,T2,T3,T4,T5,T6,T7,T8}
     base_prob::GrossPitaevskiiProblem{N,T1,T2,T3,T4,T5,T6,T7}
+    noise_func::T8
 
-    function StochasticGrossPitaevskiiProblem(dispersion!, potential!, nonlinearity, pump!, u0, lengths,
-        ::Type{form}, ::Type{Tnoise}, param=()) where {form,Tnoise}
+    function StochasticGrossPitaevskiiProblem(dispersion!, potential!, nonlinearity, pump!, u0, lengths, param,
+        noise_func::T8) where {T8}
 
-        base_prob = GrossPitaevskiiProblem(dispersion!, potential!, nonlinearity, pump!, u0, lengths, param)
+        first_u0 = u0[.., 1]
+        base_prob = GrossPitaevskiiProblem(dispersion!, potential!, nonlinearity, pump!, first_u0, lengths, param)
         N, T1, T2, T3, T4, T5, T6, T7 = typeof(base_prob).parameters
-        new{form,Tnoise,N,T1,T2,T3,T4,T5,T6,T7}(base_prob)
+        new{N,T1,T2,T3,T4,T5,T6,T7,T8}(base_prob, noise_func)
     end
 end
 
 function Base.show(io::IO,
-    ::StochasticGrossPitaevskiiProblem{form,Tnoise,N,T1}) where {form,Tnoise,N,T1}
+    ::StochasticGrossPitaevskiiProblem{N,T1}) where {N,T1}
     print(io, "$(N)D StochasticGrossPitaevskiiProblem{$T1}")
 end
+
+function Base.getproperty(prob::StochasticGrossPitaevskiiProblem, s::Symbol)
+    if hasproperty(prob.base_prob, s)
+        getproperty(prob.base_prob, s)
+    else
+        getproperty(prob, s)
+    end
+end
+
