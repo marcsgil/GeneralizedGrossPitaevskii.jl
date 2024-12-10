60e845d11ef997d9e922d0f2edab6de9dbc64fe3
Marcos Gil <marcosgildeoliveira@gmail.com>
2024-12-10 17:36:04 +0100

    added damping potentials; worked more on cavity

--- examples/1D_cavity.jl
+++ examples/1D_cavity.jl
@@ -3,10 +3,15 @@ using GeneralizedGrossPitaevskii
 using CairoMakie, StructuredLight
 
 const ħ = 0.6582f0 #meV.ps
+const L = 800.0f0
 
 function dispersion(ks, param)
     δ₀, m, γ = param
     -im * γ / 2 + ħ * sum(abs2, ks) / 2m - δ₀
+end
+
+function potential(rs, param)
+    100 * damping_potential(rs, -L / 2, L / 2, 10) #- 0.85 / ħ * exp(-sum(abs2, rs) / 0.75^2)
 end
 
 function I(t, t_cycle, Imax)
@@ -14,18 +19,25 @@ function I(t, t_cycle, Imax)
     val < 0 ? zero(val) : val
 end
 
+function plato_pump(x, xmin, xmax, width)
+    x̅ = (xmin + xmax) / 2
+    if x ≤ x̅
+        1 + tanh((x - xmin) / width)
+    else
+        1 - tanh((x - xmax) / width)
+    end
+end
+
 function bistability_cycle(g, δ₀, m, γ, kₚ, L, N, Imax, t_cycle, t_end, solver)
     lengths = (L,)
 
     u0 = zeros(ComplexF32, ntuple(n -> N, length(lengths)))
 
-    function potential(rs, param)
-        100 * damping_potential(rs, -L / 2, L / 2, 1)
-    end
-
     function pump(x, param, t)
         δ₀, m, γ, kₚ, t_cycle, Imax = param
-        (x[1] ≤ 0) * √I(t, t_cycle, Imax) * cis(kₚ * x[1])
+        (x[1] ≤ -7) * √I(t, t_cycle, Imax) * cis(kₚ * x[1])
+        #(abs(x[1]) ≤ 200) * √I(t, t_cycle, Imax) #* cis(kₚ * x[1])
+        #exp(-sum(abs2,x) / 200^2) * √I(t, t_cycle, Imax) * cis(-kₚ * x[1])
     end
 
     param = (δ₀, m, γ, kₚ, t_cycle, Imax)
@@ -46,20 +58,17 @@ m = ħ^2 / (2 * 1.29f0)
 δ₀ = ωₚ - ω₀
 δ = δ₀ - ħ * kₚ^2 / 2m
 
-L = 800.0f0
 N = 256
 
 rs = range(; start=-L / 2, step=L / N, length=N)
 
 Imax = 90.0f0
-t_cycle = 3400
-t_end = 3400
-solver = StrangSplitting(1024, 1f0/2)
+t_cycle = 10000
+t_end = 9500
+solver = StrangSplitting(1024, 1.0f0)
 
 ts, sol = bistability_cycle(g, δ₀, m, γ, kₚ, L, N, Imax, t_cycle, t_end, solver)
-heatmap(rs, ts, abs2.(sol))
-
-ts
+heatmap(rs, ts, (abs2.(sol)))
 ##
 Is = I.(ts, t_cycle, Imax)
 color = [n ≤ length(ts) / 2 ? :red : :black for n ∈ eachindex(ts)]
@@ -71,6 +80,7 @@ end
 ns_theo = LinRange(0, 1800, 512)
 Is_theo = [bistability_curve(n, δ, g, γ) for n ∈ ns_theo]
 ns = abs2.(sol[N÷4, :])
+#ns = abs2.(sol[N÷2, :])
 
 with_theme(theme_latexfonts()) do
     fig = Figure(fontsize=24)
@@ -82,4 +92,43 @@ with_theme(theme_latexfonts()) do
     fig
 end
 ##
-ts[end-2] - ts[end-3]
+function speed_of_sound(g, n₀, δ, m)
+    √(ħ * (2 * g * n₀ - δ) / m)
+end
+
+function dispersion_relation(k, kₚ, g, n₀, δ, m, branch::Bool)
+    cₛ = speed_of_sound(g, n₀, δ, m)
+    v = ħ * kₚ / m
+    pm = branch ? 1 : -1
+    gn₀ = g * n₀
+    v * k + pm * √(ħ^2 * k^4 / 4m^2 + (cₛ * k)^2 + (gn₀ - δ) * (3gn₀ - δ))
+end
+
+ks = LinRange(-1, 2, 512)
+
+ωs₊ = dispersion_relation.(ks, kₚ, g, ns[end], δ, m, true)
+ωs₋ = dispersion_relation.(ks, kₚ, g, ns[end], δ, m, false)
+
+fig = Figure()
+ax = Axis(fig[1, 1]; xlabel=L"k", ylabel=L"\omega")
+ylims!(-1, 1)
+lines!(ax, ks, ωs₊)
+lines!(ax, ks, ωs₋)
+fig
+##
+u0 = sol[:, end]
+
+function pump(x, param, t)
+    δ₀, m, γ, kₚ, t_cycle, Imax, t_end = param
+    ((x[1] ≤ 0) * cis(kₚ * x[1])) * √I(t_end, t_cycle, Imax)
+end
+
+param = (δ₀, m, γ, kₚ, t_cycle, Imax, t_end)
+prob = GrossPitaevskiiProblem(u0, (L,), dispersion, potential, g, pump, param)
+
+ts, sol = solve(prob, solver, (0, 1000))
+
+heatmap(rs, ts, abs2.(sol))
+##
+lines(rs, abs2.(sol[:, end]))
+##
--- src/misc.jl
+++ src/misc.jl
@@ -24,11 +24,15 @@ end
 
 grid_map!(::Nothing, f!, rs::NTuple{M}, args...) where {M} = nothing
 
-"""function damping_potential_base(x, xmin, xmax, width)
+function damping_potential_base(x, xmin, xmax, width)
     exp(-(x - xmin)^2 / width^2) + exp(-(x - xmax)^2 / width^2)
 end
 
-pot_reduction(Vs) = 2 * sum(Vs) / (1 + prod(Vs)) / length(Vs)
+function damping_potential(x::NTuple{1}, xmin, xmax, widht)
+    -im * damping_potential_base(x[1], xmin, xmax, widht)
+end
+
+"""pot_reduction(Vs) = 2 * sum(Vs) / (1 + prod(Vs)) / length(Vs)
 
 first_or_getindex(x::Number, inds...) = x
 first_or_getindex(x, inds...) = x[inds...]"""
@@ -55,7 +59,7 @@ end"""
 smooth_min(x, y; β=5) = ((x + y) - √((x - y)^2 + 1 / β)) / 2
 smooth_min(x, y, args...; β=5) = smooth_min(smooth_min(x, y; β), args...; β)
 
-function damping_potential(x, xmin, xmax, width)
+"""function damping_potential(x, xmin, xmax, width)
     dist_min = smooth_min(abs.(x .- xmin)..., abs.(x .- xmax)...)
     -im * exp(-dist_min^2 / width^2)
-end
+end"""
