0e198d88bfcd4db1d2918f04066350d10b6cf376
Marcos Gil <marcosgildeoliveira@gmail.com>
2025-03-17 18:26:21 +0100

    removed potentials

--- src/GeneralizedGrossPitaevskii.jl
+++ src/GeneralizedGrossPitaevskii.jl
@@ -10,9 +10,6 @@ export GrossPitaevskiiProblem
 
 include("misc.jl")
 
-include("potentials.jl")
-export damping_potential
-
 include("kernels.jl")
 
 include("fixed_time_step_splitting_solvers.jl")
