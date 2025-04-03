6b158c29d4064e8585646d41a72ccc67af289ceb
Marcos Gil <marcosgildeoliveira@gmail.com>
2025-04-03 15:26:13 +0200

    only export strang splitting

--- src/GeneralizedGrossPitaevskii.jl
+++ src/GeneralizedGrossPitaevskii.jl
@@ -16,10 +16,8 @@ include("strang_splitting.jl")
 export StrangSplitting
 
 include("simple_alg.jl")
-export SimpleAlg
 
 include("exp_finite_dif.jl")
-export ExpFiniteDiff
 
 include("fixed_time_stepping.jl")
 export solve
